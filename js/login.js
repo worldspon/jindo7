d3d3fa9446e06396cd0fdbcdbaaa66feb10651b4
repeto1204 <45305363+repeto1204@users.noreply.github.com>
2019-04-08 16:04:05 +0900

    author : JJH | message : login mobile height js 추가

---
+++ js/login.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let body = document.body;
+let html = document.documentElement;
+
+let main = document.querySelector('main');
+
+console.log(main.offsetHeight);
+
+window.addEventListener('resize', function() {
+    if(window.innerHeight <= main.offsetHeight) {
+        body.style.height = 'auto';
+        html.style.height = 'auto';
+    } else {
+        body.style.height = '100%';
+        html.style.height = '100%';
+    }
+    console.log(main.offsetHeight);
+});
