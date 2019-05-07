461902130153a70617a5147e9f1e471a2d3f04c2
yueunkyung <48663193+yueunkyung@users.noreply.github.com>
2019-05-07 16:48:07 +0900

    author : eunkyung | message : 공지사항 답변 노트 추가, 내 배팅결과 좀비낙하 추가

--- js/myqcontent.js
+++ js/myqcontent.js
@@ -4,3 +4,8 @@ const listBtn = document.querySelector('.list-btn');
 listBtn.addEventListener('click', function() {
     window.history.back();
 });
+
+$('#summernote').summernote({
+    placeholder: '답변을 작성하세요.',
+    tabsize: 2
+});
