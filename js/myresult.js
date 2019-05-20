d4ab4e356444932d4bde12b5095c9862f99dc4ea
repeto1204 <45305363+repeto1204@users.noreply.github.com>
2019-05-20 17:15:09 +0900

    author : JJH | message : 내 베팅결과 마크업 수정 및 Javascript 작성, 통신테스트 완료

--- js/myresult.js
+++ js/myresult.js
@@ -2,20 +2,162 @@
 
 const myResultContentBox = document.querySelector('.myresult-content-box');
 const gameCategoryButton = document.querySelectorAll('.mybet-content-box > button');
+let _fnc;
 
-gameCategoryButton.forEach((gameButton)=>{
-    gameButton.addEventListener('click', function() {
+// add,remove event 하기 위해 함수 선언
+bindClickEventFunction();
 
-        setColorClass(this);
+// 첫 접속시 좀비레이스 테이블 생성
+createFirstTable();
 
-        if(this.innerText == '좀비낙하') {
-            createDropTable(this.innerText);
+
+/**
+ * 
+ * @brief event add, remove를 위해 _fnc 변수에 함수를 추가
+ * @author JJH
+ * @see 참고사항
+ * 
+ */
+function bindClickEventFunction() {
+    _fnc = function(e) {
+
+        setColorClass(e.target);
+    
+        if(e.target.innerText == '좀비낙하') {
+            let data = gameResultPromise('GET', 'http://192.168.0.24:8080/game/myBet/zombieDrop');
+            data.then((data)=>{
+                gameCategoryButton.forEach((gameButton)=>{
+                    gameButton.removeEventListener('click', _fnc)
+                })
+                data = JSON.parse(data);
+                if(data.errorCode == 0) {
+                    createDropTable(data.drop);
+                }
+                gameCategoryButton.forEach((gameButton)=>{
+                    if(e.target.innerText != gameButton.innerText) {
+                        gameButton.addEventListener('click', _fnc);
+                    }
+                })
+            }, (err)=>{
+                alert(err);
+            });
+    
+        }else if(e.target.innerText == '좀비격투') {
+            let data = gameResultPromise('GET', 'http://192.168.0.24:8080/game/myBet/zombieFight');
+            data.then((data)=>{
+                gameCategoryButton.forEach((gameButton)=>{
+                    gameButton.removeEventListener('click', _fnc)
+                })
+                data = JSON.parse(data);
+                if(data.errorCode == 0) {
+                    createFightTable(data.fight);
+                }
+                gameCategoryButton.forEach((gameButton)=>{
+                    if(e.target.innerText != gameButton.innerText) {
+                        gameButton.addEventListener('click', _fnc);
+                    }
+                })
+            }, (err)=>{
+                alert(err);
+            });
+        } else if(e.target.innerText == '좀비격파') {
+    
+            let data = gameResultPromise('GET', 'http://192.168.0.24:8080/game/myBet/zombieBreak');
+            data.then((data)=>{
+                gameCategoryButton.forEach((gameButton)=>{
+                    gameButton.removeEventListener('click', _fnc)
+                })
+                data = JSON.parse(data);
+                if(data.errorCode == 0) {
+                    createTable(data.break, e.target.innerText);
+                }
+                gameCategoryButton.forEach((gameButton)=>{
+                    if(e.target.innerText != gameButton.innerText) {
+                        gameButton.addEventListener('click', _fnc);
+                    }
+                })
+            }, (err)=>{
+                alert(err);
+            });
+    
         } else {
-            createTable(this.innerText);
+            
+            let data = gameResultPromise('GET', 'http://192.168.0.24:8080/game/myBet/zombieRace');
+            data.then((data)=>{
+                gameCategoryButton.forEach((gameButton)=>{
+                    gameButton.removeEventListener('click', _fnc)
+                })
+                data = JSON.parse(data);
+                if(data.errorCode == 0) {
+                    createTable(data.race, e.target.innerText);
+                }
+                gameCategoryButton.forEach((gameButton)=>{
+                    if(e.target.innerText != gameButton.innerText) {
+                        gameButton.addEventListener('click', _fnc);
+                    }
+                })
+            }, (err)=>{
+                alert(err);
+            });
         }
-    })
-})
+    }
+}
 
+
+/**
+ * 
+ * @brief 첫 접속시 좀비레이스 테이블 생성
+ * @author JJH
+ * @see 참고사항
+ * 
+ */
+function createFirstTable() {
+    let data = gameResultPromise('GET', 'http://192.168.0.24:8080/game/myBet/zombieRace');
+    data.then((data)=>{
+        gameCategoryButton.forEach((gameButton)=>{
+            gameButton.removeEventListener('click', _fnc)
+        })
+        data = JSON.parse(data);
+        if(data.errorCode == 0) {
+            createTable(data.race, '좀비레이스');
+        }
+        gameCategoryButton.forEach((gameButton)=>{
+            if('좀비레이스' != gameButton.innerText) {
+                gameButton.addEventListener('click', _fnc);
+            }
+        })
+    }, (err)=>{
+        alert(err);
+    });
+}
+ 
+
+/**
+ * 
+ * @brief 비동기 통신
+ * @author JJH
+ * @param type 통신 방식
+ * @param url 통신 할 URL
+ * 
+ */
+function gameResultPromise(type, url) {
+    return new Promise((resolve, reject)=>{
+        const xhr = new XMLHttpRequest();
+        xhr.open(type, url);
+        xhr.onload = () => resolve(xhr.responseText);
+        xhr.onerror = () => reject(xhr.statusText);
+        xhr.send();
+    })
+}
+
+/**
+ * 
+ * @brief 선택된 버튼에 class를 추가하여 색 변경
+ * 버튼 객체를 전부 on class 해제 후 선택 버튼에 class 추가
+ * @author JJH
+ * @param clickedButton 클릭 된 버튼객체
+ * 
+ */
 function setColorClass(clickedButton) {
     gameCategoryButton.forEach((nonClickedButton)=>{
         nonClickedButton.classList.remove('on');
@@ -23,116 +165,147 @@ function setColorClass(clickedButton) {
     clickedButton.classList.add('on');
 }
 
-function createTable(gameTitle) {
-    myResultContentBox.innerHTML = `<table>
-    <caption class="tb-main-title">${gameTitle} 결과표 내용</caption>
+
+/**
+ * 
+ * @brief 좀비레이스, 좀비격파 테이블 생성
+ * @author JJH
+ * @param data 해당 JSON 데이터
+ * @param gameTitle 해당 게임 타이틀
+ * 
+ */
+function createTable(data, gameTitle) {
+
+    let dummyText = '';
+
+    dummyText = 
+    `<table>
+        <caption class="tb-main-title">${gameTitle} 베팅기록</caption>
+        <thead>
+            <tr class="myresult-small-title one-line">
+            <th class="date-col">일시</th>
+                <th class="count-star">회차</th> 
+                <th class="betting-zombie">베팅한 <span class="more-spare">좀비</span></th>
+                <th>베팅<span class="more-spare">쿠폰</span></th>
+                <th class="game-money-col">당첨 <span class="more-spare">게임머니</span></th>
+                <th class="rank-col">당첨 <span class="more-spare">랭킹점수</span></th>
+                <th class="result-grade">결과<span class="more-spare">(등수)</span></th>
+            </tr>
+        </thead>
+    <tbody>`;
+
+    for(let el of data) {
+        dummyText +=
+        `<tr class="one-line border-bottom">
+            <td>${el.dateTime}</td>
+            <td>${el.gameCount}</td>
+            <td>${el.zombieName}</td>
+            <td>${el.coupon}</td>
+            <td>${el.resultGameMoney}</td>
+            <td>${el.resultRank}</td>
+            <td>${el.result}</td>
+        </tr>`
+    }
+
+    dummyText += `</tbody></table>`;
+
+    myResultContentBox.innerHTML = dummyText;
+}
+
+
+/**
+ * 
+ * @brief 좀비격투 테이블 생성
+ * @author JJH
+ * @param data 해당 JSON 데이터
+ * 
+ */
+function createFightTable(data) {
+    let dummyText ='';
+
+    dummyText = `<table>
+    <caption class="tb-main-title">좀비격투 베팅기록</caption>
     <thead>
         <tr class="myresult-small-title one-line">
-            <th>일시</th>
+            <th class="date-col">일시</th>
             <th class="count-star">회차</th> 
-            <th>베팅한 <span class="more-spare">좀비</span></th>
-            <th>베팅쿠폰</th>
-            <th>당첨 <span class="more-spare">게임머니</span></th>
-            <th>당첨 <span class="more-spare">랭킹점수</span></th>
+            <th class="betting-zombie">베팅한 <span class="more-spare">좀비</span></th>
+            <th>KO옵션</th>
+            <th>베팅<span class="more-spare">쿠폰</span></th>
+            <th class="game-money-col">당첨 <span class="more-spare">게임머니</span></th>
+            <th class="rank-col">당첨 <span class="more-spare">랭킹점수</span></th>
             <th class="result-grade">결과<span class="more-spare">(등수)</span></th>
         </tr>
     </thead>
-    <tbody>
-        <tr class="one-line border-bottom">
-            <td>19.05.02</td>
-            <td>488</td>
-            <td>코코엄마</td>
-            <td>1</td>
-            <td>38,000</td>
-            <td>12,800</td>
-            <td>1</td>
-        </tr>
-        <tr class="one-line border-bottom">
-            <td>19.05.02</td>
-            <td>16</td>
-            <td>군인</td>
-            <td>2</td>
-            <td>24,000</td>
-            <td>5,000</td>
-            <td>무승부</td>
-        </tr>
-        <tr class="one-line border-bottom">
-            <td>19.05.02</td>
-            <td>16</td>
-            <td>피아니스트</td>
-            <td>2</td>
-            <td>0</td>
-            <td>0</td>
-            <td>KO</td>
-        </tr>
-        <tr class="one-line border-bottom">
-            <td>19.05.02</td>
-            <td>16</td>
-            <td>교통경찰</td>
-            <td>2</td>
-            <td>0</td>
-            <td>0</td>
-            <td>판정승</td>
-        </tr>
-        <tr class="one-line border-bottom">
-            <td>19.05.02</td>
-            <td>16</td>
-            <td>간호사</td>
-            <td>2</td>
-            <td>0</td>
-            <td>0</td>
-            <td>5</td>
-        </tr>
-    </tbody>
-</table>`;
+    <tbody>`;
+
+    for(let el of data) {
+        dummyText += 
+        `<tr class="one-line border-bottom">
+            <td>${el.dateTime}</td>
+            <td>${el.gameCount}</td>
+            <td>${el.zombieName}</td>
+            <td>${el.koChoice}</td>
+            <td>${el.coupon}</td>
+            <td>${el.resultGameMoney}</td>
+            <td>${el.resultRank}</td>
+            <td>${el.gameResult}</td>
+        </tr>`;
+    }
+
+    dummyText += `</tbody></table>`;
+
+    
+
+    myResultContentBox.innerHTML = dummyText;
 }
 
-function createDropTable(gameTitle) {
-    myResultContentBox.innerHTML = `<table>
-    <caption class="tb-main-title">${gameTitle} 결과표 내용</caption>
+
+/**
+ * 
+ * @brief 좀비낙하 테이블 생성
+ * @author JJH
+ * @param data 해당 JSON 데이터
+ * 
+ */
+function createDropTable(data) {
+
+    let dummyText = '';
+
+    dummyText = `<table>
+    <caption class="tb-main-title">좀비낙하 베팅기록</caption>
     <thead>
         <tr class="myresult-small-title one-line">
-            <th class="drop-date">일시</th>
-            <th class="drop-times">회차</th>
-            <th class="selected-num">선택번호</th>
+            <th class="date-col">일시</th>
+            <th class="count-star">회차</th> 
+            <th class="choice-number">선택번호</th> 
             <th class="goal-times">당첨수</th>
-            <th class="goal-money">당첨 <span class="more-spare">게임머니</span></th>
-            <th class="goal-spons">당첨 <span class="more-spare">스폰</span></th>
-            <th class="goal-rank">당첨 <span class="more-spare">랭킹점수</span></th>
-            <th class="high-grade">최고<span class="more-spare">등수</span></th>
+            <th class="game-money-col">당첨 <span class="more-spare">게임머니</span></th>
+            <th>당첨 <span class="more-spare">스폰</span></th>
+            <th class="rank-col">당첨 <span class="more-spare">랭킹점수</span></th>
+            <th class="result-grade">최고<span class="more-spare">등수</span></th>
         </tr>
     </thead>
-    <tbody>
-        <tr class="one-line border-bottom">
-            <td>2019.05.07</td>
-            <td>288</td>
-            <td>14/16/20/22/26</td>
-            <td>5</td>
-            <td>100,000</td>
-            <td>100</td>
-            <td>200,000</td>
-            <td>5등</td>
-        </tr>
-        <tr class="one-line border-bottom">
-            <td>2019.05.07</td>
-            <td>288</td>
-            <td>5/19/22/27/28</td>
-            <td>4</td>
-            <td>100,000</td>
-            <td>100</td>
-            <td>200,000</td>
-            <td>5등</td>
-        </tr>
-        <tr class="one-line border-bottom">
-            <td>2019.05.07</td>
-            <td>288</td>
-            <td>14/16/20/22/26</td>
-            <td>5</td>
-            <td>100,000</td>
-            <td>100</td>
-            <td>200,000</td>
-            <td>꽝</td>
-        </tr>
-    </tbody>
-</table>`;
+    <tbody>`;
+
+    for(let el of data) {
+        dummyText +=
+        `<tr class="one-line border-bottom">
+            <td>${el.dateTime}</td>
+            <td>${el.gameCount}</td>
+            <td>${el.result}</td>
+            <td>${el.matchPoint}</td>
+            <td>${el.prizeGameMoney}</td>
+            <td>${el.prizeSpon}</td>
+            <td>${el.prizeRank}</td>
+            <td>${el.grade}</td>
+        </tr>`;
+    }
+
+    dummyText +=
+    `</tbody>
+    </table>`;
+
+    myResultContentBox.innerHTML = dummyText;
+
 }
