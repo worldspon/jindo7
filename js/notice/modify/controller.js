9a25e27690772cb9ea2ef098e579a44df91d4345
repeto1204 <45305363+repeto1204@users.noreply.github.com>
2019-08-06 17:42:23 +0900

    author : JJH | message : 공지사항 수정 template font-size 수정

--- js/notice/modify/controller.js
+++ js/notice/modify/controller.js
@@ -5,6 +5,7 @@ import { EventLogic } from './model.js';
 class Init {
     static createSummerNote() {
         $('#summernote').summernote({
+            fontSize : 40,
             placeholder: '내용을 입력해주시기 바랍니다.',
             tabsize: 2,
             height: 700
